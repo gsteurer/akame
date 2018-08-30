4843cdc0908bbd8a2f13affb626994a368ea1096
Joseph Gus Steurer <gsteurer@gmail.com>
2018-08-29 23:23:24 -0500

    add CLRS rb tree delete, search, min, max

--- include/containers/redblacktree.hpp
+++ include/containers/redblacktree.hpp
@@ -21,7 +21,8 @@ template <typename T> struct Node {
 };
 
 template <class T>
-void rbLeftRotate(akame::rbtree::Node<T> **tree, akame::rbtree::Node<T> *x) {
+inline void rbLeftRotate(akame::rbtree::Node<T> **tree,
+                         akame::rbtree::Node<T> *x) {
   akame::rbtree::Node<T> *y = x->Right;
   x->Right = y->Left;
   if (y->Left != Node<T>::T_NIL) {
@@ -40,7 +41,8 @@ void rbLeftRotate(akame::rbtree::Node<T> **tree, akame::rbtree::Node<T> *x) {
 }
 
 template <class T>
-void rbRightRotate(akame::rbtree::Node<T> **tree, akame::rbtree::Node<T> *x) {
+inline void rbRightRotate(akame::rbtree::Node<T> **tree,
+                          akame::rbtree::Node<T> *x) {
   akame::rbtree::Node<T> *y = x->Left;
   x->Left = y->Right;
   if (y->Right != Node<T>::T_NIL) {
@@ -123,6 +125,137 @@ void rbInsertFixup(akame::rbtree::Node<T> **tree, akame::rbtree::Node<T> *z) {
     }
   }
   tree[0]->Color = COLOR::BLACK;
+}
+
+template <class T>
+inline void rbTransplant(akame::rbtree::Node<T> **tree,
+                         akame::rbtree::Node<T> *u, akame::rbtree::Node<T> *v) {
+  if (u->Parent == Node<T>::T_NIL) {
+    *tree = v;
+  } else if (u == u->Parent->Left) {
+    u->Parent->Left = v;
+  } else {
+    u->Parent->Right = v;
+  }
+  v->Parent = u->Parent;
+}
+
+template <class T>
+inline void rbDelete(akame::rbtree::Node<T> **tree, akame::rbtree::Node<T> *z) {
+  akame::rbtree::Node<T> *y = z;
+  COLOR y_original_color = y->Color;
+  akame::rbtree::Node<T> *x;
+  if (z->Left == Node<T>::T_NIL) {
+    x = z->Right;
+    rbTransplant(tree, z, z->Right);
+  } else if (z->Right == Node<T>::T_NIL) {
+    x = z->Left;
+    rbTransplant(tree, z, z->Left);
+  } else {
+    y = rbTreeMinimum(z->Right);
+    y_original_color = y->Color;
+    x = y->Right;
+    if (y->Parent == z) {
+      x->Parent = y;
+    } else {
+      rbTransplant(tree, y, y->Right);
+      y->Right = z->Right;
+      y->Right->Parent = y;
+    }
+    rbTransplant(tree, z, y);
+    y->Left = z->Left;
+    y->Left->Parent = y;
+    y->Color = z->Color;
+  }
+
+  if (y_original_color == COLOR::BLACK) {
+    rbDeleteFixup(tree, x);
+  }
+}
+
+template <class T>
+inline void rbDeleteFixup(akame::rbtree::Node<T> **tree,
+                          akame::rbtree::Node<T> *x) {
+  while (x != *tree && x->Color == COLOR::BLACK && x != Node<T>::T_NIL) {
+    if (x == x->Parent->Left) {
+      akame::rbtree::Node<T> *w = x->Parent->Right;
+      if (w->Color == COLOR::RED) {
+        w->Color = COLOR::BLACK;
+        x->Parent->Color = COLOR::RED;
+        rbLeftRotate(tree, x->Parent);
+        w = x->Parent->Right;
+      }
+      if (w->Left->Color == COLOR::BLACK && w->Right->Color == COLOR::BLACK) {
+        w->Color = COLOR::RED;
+        x = x->Parent;
+      } else {
+        if (w->Right->Color == COLOR::BLACK) {
+          w->Left->Color = COLOR::BLACK;
+          w->Color = COLOR::RED;
+          rbRightRotate(tree, w);
+          w = w->Parent->Right;
+        }
+        w->Color = x->Parent->Color;
+        x->Parent->Color = COLOR::BLACK;
+        w->Right->Color = COLOR::BLACK;
+        rbLeftRotate(tree, x->Parent);
+        x = *tree;
+      }
+    } else {
+      akame::rbtree::Node<T> *w = x->Parent->Left;
+      if (w->Color == COLOR::RED) {
+        w->Color = COLOR::BLACK;
+        x->Parent->Color = COLOR::RED;
+        rbRightRotate(tree, x->Parent);
+        w = x->Parent->Left;
+      }
+      if (w->Right->Color == COLOR::BLACK && w->Left->Color == COLOR::BLACK) {
+        w->Color = COLOR::RED;
+        x = x->Parent;
+      } else {
+        if (w->Left->Color == COLOR::BLACK) {
+          w->Right->Color = COLOR::BLACK;
+          w->Color = COLOR::RED;
+          rbLeftRotate(tree, w);
+          w = w->Parent->Left;
+        }
+        w->Color = x->Parent->Color;
+        x->Parent->Color = COLOR::BLACK;
+        w->Left->Color = COLOR::BLACK;
+        rbRightRotate(tree, x->Parent);
+        x = *tree;
+      }
+    }
+  }
+  x->Color = COLOR::BLACK;
+}
+
+template <class T>
+inline akame::rbtree::Node<T> *rbTreeMinimum(akame::rbtree::Node<T> *x) {
+  while (x->Left != Node<T>::T_NIL) {
+    x = x->Left;
+  }
+  return x;
+}
+
+template <class T>
+inline akame::rbtree::Node<T> *rbTreeMaximum(akame::rbtree::Node<T> *x) {
+  while (x->Right != Node<T>::T_NIL) {
+    x = x->Right;
+  }
+  return x;
+}
+
+template <class T>
+inline akame::rbtree::Node<T> *rbTreeSearch(akame::rbtree::Node<T> *x, T k) {
+  while (x != Node<T>::T_NIL && k != x->Key) {
+    if (k < x->Key) {
+      x = x->Left;
+    } else {
+      x = x->Right;
+    }
+  }
+  return x;
 }
 
 } // namespace rbtree
--- tests/containers/redblacktree/redblacktree_test.cpp
+++ tests/containers/redblacktree/redblacktree_test.cpp
@@ -13,12 +13,61 @@ template <>
 akame::rbtree::Node<int> *akame::rbtree::Node<int>::T_NIL =
     &akame::rbtree::Node<int>::T_NULL_NODE;
 
-TEST(RedBlackTree, Test1) {
-  srand(time(NULL));
-  akame::rbtree::Node<int>::T_NULL_NODE.Key = 69;
-  akame::rbtree::Node<int> *tree = akame::rbtree::Node<int>::T_NIL;
+template <>
+akame::rbtree::Node<float>
+    akame::rbtree::Node<float>::T_NULL_NODE = akame::rbtree::Node<float>();
 
-  for (int idx = 0; idx < 10000; idx++) {
-    akame::rbtree::rbInsert(&tree, rand() % 1000);
+template <>
+akame::rbtree::Node<float> *akame::rbtree::Node<float>::T_NIL =
+    &akame::rbtree::Node<float>::T_NULL_NODE;
+
+TEST(RedBlackTree, Test1) {
+  akame::rbtree::Node<int> *tree = akame::rbtree::Node<int>::T_NIL;
+  for (int idx = 0; idx < 10; idx++) {
+    akame::rbtree::rbInsert(&tree, idx + 1);
   }
+
+  for (int idx = 0; idx < 10; idx++) {
+    auto node = akame::rbtree::rbTreeSearch(tree, idx + 1);
+    ASSERT_EQ(node->Key, idx + 1);
+  }
+  ASSERT_EQ(akame::rbtree::rbTreeMinimum(tree)->Key, 1);
+  ASSERT_EQ(akame::rbtree::rbTreeMaximum(tree)->Key, 10);
+
+  for (int idx = 9; idx >= 0; idx--) {
+    auto node = akame::rbtree::rbTreeSearch(tree, idx + 1);
+    akame::rbtree::rbDelete(&tree, node);
+  }
+  ASSERT_EQ(tree, akame::rbtree::Node<int>::T_NIL);
 }
+
+TEST(RedBlackTree, Test2) {
+  srand(time(NULL));
+  akame::rbtree::Node<int> *tree = akame::rbtree::Node<int>::T_NIL;
+  for (int idx = 0; idx < 10000; idx++) {
+    akame::rbtree::rbInsert(&tree, (rand() % 1000) + 1);
+  }
+  akame::rbtree::rbInsert(&tree, 1);
+  akame::rbtree::rbInsert(&tree, 1002);
+  ASSERT_EQ(akame::rbtree::rbTreeMinimum(tree)->Key, 1);
+  ASSERT_EQ(akame::rbtree::rbTreeMaximum(tree)->Key, 1002);
+
+  //@@@todo complete
+  /*
+    for (int idx = 0; idx < 10; idx++) {
+          auto node = akame::rbtree::rbTreeSearch(tree, idx+1);
+          ASSERT_EQ(node->Key, idx+1);
+    }
+    ASSERT_EQ(akame::rbtree::rbTreeMinimum(tree)->Key, 1);
+    ASSERT_EQ(akame::rbtree::rbTreeMaximum(tree)->Key, 10);
+
+    for (int idx = 9; idx >= 0; idx--) {
+          auto node = akame::rbtree::rbTreeSearch(tree, idx+1);
+          akame::rbtree::rbDelete(&tree, node);
+    }
+    ASSERT_EQ(tree, akame::rbtree::Node<int>::T_NIL);
+  */
+}
+
+//@@@ todo test tree properties
+//@@@ todo what happens when deleting something with same key as null node?
